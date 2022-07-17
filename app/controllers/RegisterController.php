db28aaa01e665fed064d6496447f1cca93180e65
Nikolenko_Artem <artgomartgom@yandex.ru>
2022-07-18 01:49:07 +0500

    Добавлена валидация при отправке формы на странице регистрации

--- app/controllers/RegisterController.php
+++ app/controllers/RegisterController.php
@@ -3,6 +3,8 @@
 namespace app\controllers;
 
 use app\core\Controller;
+use app\core\Request;
+use app\models\form\RegisterForm;
 
 /**
  * Class RegisterController
@@ -13,10 +15,20 @@ class RegisterController extends Controller
      * Register page
      * @return string|string[]
      */
-    public function index()
+    public function index(Request $request)
     {
+        $model = new RegisterForm();
+
+        if ($request->getMethod() === 'post') {
+            $model->loadData($request->getBody());
+            if ($model->validate()) {
+                return $model->register();
+            }
+        }
+
         return $this->render('auth/register', [
-            'title' => 'Регистрация'
+            'title' => 'Регистрация',
+            'model' => $model
         ]);
     }
 }
