06ec82a0213684e249fc9c6142c280f38bef4036
Nikolenko_Artem <artgomartgom@yandex.ru>
2022-07-17 23:12:49 +0500

    Правки с опциями PDO в классе Database

--- app/core/db/Database.php
+++ app/core/db/Database.php
@@ -32,11 +32,15 @@ class Database
                 $username = $dbConfig['user'] ?? '';
                 $password = $dbConfig['password'] ?? '';
 
+                $options = [
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"
+                ];
+
                 self::$pdo = new PDO(
                     $dbDsn,
                     $username,
                     $password,
-                    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")
+                    $options
                 );
                 self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
             } catch (PDOException $e) {
