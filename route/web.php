372684355bbba2a7dbcc46262d4b388d23249f9a
Nikolenko_Artem <artgomartgom@yandex.ru>
2022-07-19 01:03:53 +0500

    Добавлен роут выхода из аккаунта

--- route/web.php
+++ route/web.php
@@ -11,6 +11,8 @@ $app->router->get('/', [SiteController::class, 'home']);
 // route register page
 $app->router->get('/register', [RegisterController::class, 'index']);
 $app->router->post('/register', [RegisterController::class, 'index']);
-//route login page
+// route login page
 $app->router->get('/login', [LoginController::class, 'index']);
 $app->router->post('/login', [LoginController::class, 'index']);
+// route logout
+$app->router->get('/logout', [LoginController::class, 'logout']);
