738face148d2ec3d90144b41e80734ea88452d29
Nikolenko_Artem <artgomartgom@yandex.ru>
2022-07-19 01:05:54 +0500

    Добавлены посредники (middlewares)

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -2,6 +2,8 @@
 
 namespace app\core;
 
+use app\core\middlewares\BaseMiddleware;
+
 /**
  * Class Controller
  */
@@ -17,6 +19,11 @@ class Controller
      * @var string
      */
     public string $action = '';
+    /**
+     *
+     * @var \app\core\middlewares\BaseMiddleware[]
+     */
+    protected array $middlewares = [];
 
     /**
      * Save layout
@@ -32,10 +39,28 @@ class Controller
      * Show view
      * @param $view
      * @param array $params
-     * @return string|string[]
+     * @return string
      */
     public function render($view, array $params = []): string
     {
         return Application::$app->router->renderView($view, $params);
+    }
+
+    /**
+     * Register middleware
+     * @param BaseMiddleware $middleware
+     */
+    public function registerMiddleware(BaseMiddleware $middleware)
+    {
+        $this->middlewares[] = $middleware;
+    }
+
+    /**
+     * Return middlewares
+     * @return \app\core\middlewares\BaseMiddleware[]
+     */
+    public function getMiddlewares(): array
+    {
+        return $this->middlewares;
     }
 }
