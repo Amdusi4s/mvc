7ff6ec29204b1fb82032e598ba6fa047a7fc46b9
Nikolenko_Artem <artgomartgom@yandex.ru>
2022-07-18 23:02:22 +0500

    Добавлены новые правила валидации для моделей

--- app/core/Model.php
+++ app/core/Model.php
@@ -12,6 +12,7 @@ class Model
     const RULE_MIN = 'min';
     const RULE_MAX = 'max';
     const RULE_MATCH = 'match';
+    const RULE_UNIQUE = 'unique';
 
     /**
      * Errors
@@ -80,8 +81,20 @@ class Model
             self::RULE_REQUIRED => 'Поле {field} обязательно к заполнению',
             self::RULE_EMAIL => 'Поле {field} должно иметь валидный email адрес',
             self::RULE_MIN => 'Минимальное количество символов в поле {field} должно быть {min}',
-            self::RULE_MAX => 'Максимальное количество символов в поле {field} должно быть {max}'
+            self::RULE_MAX => 'Максимальное количество символов в поле {field} должно быть {max}',
+            self::RULE_MATCH => 'Поле {field} должно совпадать со значением поля {match}',
+            self::RULE_UNIQUE => 'Запись с этим {field} уже существует',
         ];
+    }
+
+    /**
+     * Return message rule
+     * @param $rule
+     * @return string
+     */
+    public function errorMessage($rule): string
+    {
+        return $this->errorMessages()[$rule];
     }
 
     /**
@@ -90,13 +103,24 @@ class Model
      * @param string $rule
      * @param array $params
      */
-    public function addError(string $attribute, string $rule, array $params = [])
+    protected function addErrorByRule(string $attribute, string $rule, array $params = [])
     {
-        $message = $this->errorMessages()[$rule] ?? '';
+        $params['field'] ??= $this->getLabel($attribute);
+        $errorMessage = $this->errorMessage($rule);
         foreach ($params as $key => $value) {
-            $message = str_replace("{{$key}}", $value, $message);
+            $errorMessage = str_replace("{{$key}}", $value, $errorMessage);
         }
-        $message = str_replace("{field}", $this->getLabel($attribute), $message);
+        $this->errors[$attribute][] = $errorMessage;
+    }
+
+
+    /**
+     * Add error rule
+     * @param string $attribute
+     * @param string $message
+     */
+    public function addError(string $attribute, string $message)
+    {
         $this->errors[$attribute][] = $message;
     }
 
@@ -114,19 +138,29 @@ class Model
                     $ruleName = $rule[0];
                 }
                 if ($ruleName === self::RULE_REQUIRED && !$value) {
-                    $this->addError($attribute, self::RULE_REQUIRED);
+                    $this->addErrorByRule($attribute, self::RULE_REQUIRED);
                 }
                 if ($ruleName === self::RULE_EMAIL && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
-                    $this->addError($attribute, self::RULE_EMAIL);
+                    $this->addErrorByRule($attribute, self::RULE_EMAIL);
                 }
                 if ($ruleName === self::RULE_MIN && mb_strlen($value) < $rule['min']) {
-                    $this->addError($attribute, self::RULE_MIN, $rule);
+                    $this->addErrorByRule($attribute, self::RULE_MIN, $rule);
                 }
                 if ($ruleName === self::RULE_MAX && mb_strlen($value) > $rule['max']) {
-                    $this->addError($attribute, self::RULE_MAX, $rule);
+                    $this->addErrorByRule($attribute, self::RULE_MAX, $rule);
                 }
                 if ($ruleName === self::RULE_MATCH && $value !== $this->{$rule['match']}) {
-                    $this->addError($attribute, self::RULE_MATCH, $rule);
+                    $this->addErrorByRule($attribute, self::RULE_MATCH, $rule);
+                }
+                if ($ruleName === self::RULE_UNIQUE) {
+                    $className = $rule['class'];
+                    $uniqueAttr = $rule['attribute'] ?? $attribute;
+                    $tableName = $className::tableName();
+                    $db = Application::$app->db;
+                    $record = $db->getRow("SELECT * FROM `{$tableName}` WHERE `{$uniqueAttr}` = :$uniqueAttr", ["$uniqueAttr" => $value]);
+                    if ($record) {
+                        $this->addErrorByRule($attribute, self::RULE_UNIQUE);
+                    }
                 }
             }
         }
