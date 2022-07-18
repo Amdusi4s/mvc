5d86b45c1291d5c4d5954f9056727ca33795e21f
Nikolenko_Artem <artgomartgom@yandex.ru>
2022-07-18 11:49:39 +0500

    добавлена ссылка на авторизацию в шаблоне регистрации

--- views/auth/register.php
+++ views/auth/register.php
@@ -13,11 +13,12 @@ $this->title = $title;
             <div class="register__form">
                 <?php $form = Form::begin('/register', 'post') ?>
                     <?php echo $form->field($model, 'name') ?>
-                <?php echo $form->field($model, 'password')->passwordField() ?>
-                <?php echo $form->field($model, 'email')->emailField() ?>
-                <button type="submit" class="btn btn-primary btn-block">Зарегистрироваться</button>
+                    <?php echo $form->field($model, 'password')->passwordField() ?>
+                    <?php echo $form->field($model, 'email')->emailField() ?>
+                    <button type="submit" class="btn btn-primary btn-block">Зарегистрироваться</button>
                 <?php Form::end() ?>
             </div>
+            <p>Вы уже зарегистрированы? <a href="/login">Авторизуйтесь</a></p>
         </div>
     </div>
 </div>
